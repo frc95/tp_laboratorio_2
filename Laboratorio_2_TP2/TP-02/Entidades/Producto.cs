dff05ed48a085ab9a37bf3c009f516c493f3742c
frc95 <36265058+frc95@users.noreply.github.com>
2019-10-04 20:17:33 -0300

    Add files via upload

---
+++ Laboratorio_2_TP2/TP-02/Entidades/Producto.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2018
+{
+    /// <summary>
+    /// La clase Producto no deberá permitir que se instancien elementos de este tipo.
+    /// </summary>
+    public abstract class Producto
+    {
+        public enum EMarca
+        {
+            Serenisima, Campagnola, Arcor, Ilolay, Sancor, Pepsico
+        }
+        private EMarca marca;
+        private string codigoDeBarras;
+        private ConsoleColor colorPrimarioEmpaque;
+
+        
+
+        /// <summary>
+        /// ReadOnly: Retornará la cantidad de calorias
+        /// </summary>
+        protected abstract short CantidadCalorias { get;  }
+
+        /// <summary>
+        /// Publica todos los datos del Producto.
+        /// </summary>
+        /// <returns></returns>
+        public abstract string Mostrar();
+        
+
+        public static explicit operator string(Producto p)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            
+            sb.AppendLine("CODIGO DE BARRAS: "+p.codigoDeBarras+"\r\n");
+            sb.AppendLine("MARCA          : "+ p.marca.ToString()+"\r\n");
+            sb.AppendLine("COLOR EMPAQUE  : "+ p.colorPrimarioEmpaque.ToString()+"\r\n");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Dos productos son iguales si comparten el mismo código de barras
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator ==(Producto v1, Producto v2)
+        {
+            bool validar = false;
+            if(!Object.Equals(v1,null) && !Object.Equals(v2,null))
+            {
+                if(v1.codigoDeBarras==v2.codigoDeBarras)
+                {
+                    validar = true;
+                }
+            }
+            return validar;
+        }
+        /// <summary>
+        /// Dos productos son distintos si su código de barras es distinto
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator !=(Producto v1, Producto v2)
+        {
+            return !(v1.codigoDeBarras == v2.codigoDeBarras);
+        }
+
+        public Producto(string codigo, EMarca marca, ConsoleColor color)
+        {
+            this.codigoDeBarras = codigo;
+            this.marca = marca;
+            this.colorPrimarioEmpaque = color;
+        }
+    }
+}
