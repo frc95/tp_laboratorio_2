d9cafeb74f9183c349cfebb3339f78ae4e94ae36
frc95 <36265058+frc95@users.noreply.github.com>
2019-11-05 22:46:50 -0300

    Add files via upload

---
+++ Laboratorio_2_TP3/Amarilla.Franco.2A.TP3/PruebaUnitariaDeUniversidad/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PruebaUnitariaDeUniversidad")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PruebaUnitariaDeUniversidad")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("875983da-c246-4321-a57b-2fbc3ea6e3d8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Laboratorio_2_TP3/Amarilla.Franco.2A.TP3/PruebaUnitariaDeUniversidad/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Excepciones;
+using Clases_Instanciables;
+using EntidadesAbstractas;
+using Archivos;
+
+namespace PruebaUnitariaDeUniversidad
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        /// <summary>
+        /// Test para saber si lanza la excepcion NacionalidadInvalidaException
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(NacionalidadInvalidaException))]
+        public void TestNacionalidad()
+        {
+            Universidad uni = new Universidad();
+            Alumno a1 = new Alumno(1, "juan", "Lopez", "99999999", EntidadesAbstractas.Persona.ENacionalidad.Argentino, Universidad.EClases.Programacion);
+
+            uni += a1;
+            
+        }
+        /// <summary>
+        /// Test para saber si lanza la excepcion DniInvalidoException
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(DniInvalidoException))]
+        public void TestFormatoDNI()
+        {
+            Universidad uni = new Universidad();
+            Alumno a1 = new Alumno(1, "juan", "Lopez","zzfe", EntidadesAbstractas.Persona.ENacionalidad.Argentino, Universidad.EClases.Programacion);
+
+            uni += a1;
+        }
+        /// <summary>
+        /// Test para saber si el DNI(string) pasa a formato int
+        /// </summary>
+        [TestMethod]
+        public void TestValorNumerico()
+        {
+            Alumno a1 = new Alumno(1, "juan", "Lopez", "12345678", EntidadesAbstractas.Persona.ENacionalidad.Argentino, Universidad.EClases.Programacion);
+
+            Assert.AreEqual(a1.DNI,12345678);
+        }
+        /// <summary>
+        /// Test para saber si las clases intanciables no son nulas
+        /// </summary>
+        [TestMethod]
+        public void TestValidarValoresNulos()
+        {
+            //Alumno a1=null;
+            //Universidad uni = new Universidad();
+            //Jornada j1 = new Jornada();
+            Profesor i1 = new Profesor(1, "Juan", "Lopez", "12234456", EntidadesAbstractas.Persona.ENacionalidad.Argentino);
+            Jornada j2 = new Jornada(Universidad.EClases.Laboratorio,i1);
+            Assert.IsNotNull(j2);
+        }
+    }
+}
