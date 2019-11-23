08789a59b71549da494fe0950d7c04cf8081e791
frc95 <36265058+frc95@users.noreply.github.com>
2019-11-23 18:49:50 -0300

    Add files via upload

---
+++ Laboratorio_2_TP4/Amarilla.Franco.2A.TP4/TestCorreo/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestCorreo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestCorreo")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("78eec296-7e4f-4e75-a678-c289ac007600")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Laboratorio_2_TP4/Amarilla.Franco.2A.TP4/TestCorreo/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Entidades;
+namespace TestCorreo
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        /// <summary>
+        /// Test para verificar si la lista de paquetes esta instanciada
+        /// </summary>
+        [TestMethod]
+        public void TestPaquetes()
+        {
+            Correo c = new Correo();
+
+            Assert.IsInstanceOfType(c.Paquetes,typeof(List<Paquete>));
+        }
+
+        /// <summary>
+        /// Test para verificar si se lanza la excepcion TrackingIdRepetidoException
+        /// cuando 2 paquetes tienen la misma tracking ID 
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(TrackingIdRepetidoException))]
+        public void TestTrackingID()
+        {
+            Correo c = new Correo();
+            Paquete p1 = new Paquete("Belgrano 478","123");
+            Paquete p2 = new Paquete("Belgrano 478", "123");
+            c += p1;
+            c += p2;
+            
+        }
+    }
+}
